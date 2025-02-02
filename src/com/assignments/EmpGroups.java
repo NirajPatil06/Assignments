4cd484bfcddd08c7bbd6ec608daf803270de4825
NirajPatil06 <nirajvyas1997@gmail.com>
2025-02-02 15:52:02 +0530

    Assignment No:3

---
+++ src/com/assignments/EmpGroups.java
@@ -0,0 +1,30 @@
+package com.assignments;
+//Child can extends parents class
+public class EmpGroups extends Employees {
+	
+	
+
+	public static void main(String[] args) {
+		//Created Empty array for storing employee names and id 
+		String[] empName = new String [3];
+		int [] empId = new int [3];
+		
+        // Created Object / new copy of Employee class
+		Employees object = new Employees();
+		
+		//Storing  parent class values to empty array 
+		empName = object.EmpNames;
+		empId= object.Ids;
+		
+		//Printing data index wise
+		System.out.println("Employee Name is:"+empName[0]+ " , " + "Employee ID:"+empId[0]);
+		System.out.println("Employee Name is:"+empName[1]+ " , " + "Employee ID:"+empId[1]);
+		System.out.println("Employee Name is:"+empName[2]+ " , " + "Employee ID:"+empId[2]);
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/assignments/Employees.java
@@ -0,0 +1,12 @@
+package com.assignments;
+//Class Employee is parent class
+public class Employees {
+   
+	String [] EmpNames= {"Bharath","ABC","XYZ"};
+	int Ids[] ={1234,1235,1236};
+	
+	public static void main (String[] args) {
+		
+		
+	}
+}
